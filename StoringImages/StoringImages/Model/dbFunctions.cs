1c513370c157eb8893cb07d2c94f44e7eadc5188
Benjo69 <tolnaibenedek25@gmail.com>
2024-07-15 11:55:05 +0200

    rakos system.data.sqlite nem mukodik

---
+++ StoringImages/StoringImages/Model/dbFunctions.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace Kribo.Class
+{
+    class dBFunctions
+    {
+        public static string ConnectionStringSQLite
+        {
+            get
+            {
+                string database =
+                AppDomain.CurrentDomain.BaseDirectory +
+               "\\Database\\Contact.s3db";
+                string connectionString =
+                @"Data Source=" + Path.GetFullPath(database);
+                return connectionString;
+            }
+        }
+    }
+}
---
+++ StoringImages/StoringImages/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace StoringImages
+{
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+        }
+    }
+}
